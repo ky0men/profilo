8a119a7a95f1eaeb348a407018e4a60ff2846e8c
Pham Phu Dien <phamphudien@gmail.com>
2022-03-22 13:44:41 +0700

    push profilo

---
+++ js/script.js
@@ -0,0 +1,80 @@
+class Typewriter {
+  constructor(txtElement, words, wait = 1000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    const current = this.wordIndex % this.words.length;
+
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove character
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add character
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 200;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set deleting to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 200;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init Typewriter
+  new Typewriter(txtElement, words, wait);
+}
+AOS.init();
+
+//REMOVE DELAY ANIMATION ON MOBILE
+console.log(window.innerWidth);
+if (window.innerWidth < 480) {
+  const delay = document.querySelectorAll(".delay");
+  delay.forEach((item) => {
+    item.removeAttribute("data-aos-delay");
+  });
+  const down = document.querySelectorAll(".down");
+  down.forEach((item) => {
+    item.removeAttribute("data-aos");
+    item.setAttribute("data-aos", "fade-up");
+  });
+}
